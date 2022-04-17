11e82427863e7bea9f3d997d5f85190aa53ecc0b
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-17 18:31:20 +0300

    Exercise fully completed
    Fixed state switching saving with local storage

--- src/components/farm/Farm.jsx
+++ src/components/farm/Farm.jsx
@@ -14,7 +14,6 @@ const Farm = () => {
   // const [sheepToCows, setSheepToCows] = useState([]);
   const [renderer, setRenderer] = useState("");
 
-  
   const [cows, setCows] = useState(
     localStorage.getItem("cows") !== null
       ? JSON.parse(localStorage.getItem("cows"))
@@ -39,16 +38,9 @@ const Farm = () => {
   useEffect(() => {
     localStorage.setItem("cows", JSON.stringify(cows));
     localStorage.setItem("sheep", JSON.stringify(sheep));
-  }, [cows, sheep]);
-
-
-  useEffect(() => {
     localStorage.setItem("cowsToSheep", JSON.stringify(cowsToSheep));
-  }, [cowsToSheep]);
-
-  useEffect(() => {
     localStorage.setItem("sheepToCows", JSON.stringify(sheepToCows));
-  }, [sheepToCows]);
+  }, [cows, sheep, cowsToSheep, sheepToCows]);
 
   const fillAnimals = () => {
     const startingCows = rndNum(5, 20);
@@ -68,38 +60,39 @@ const Farm = () => {
   const removeCow = (data) => {
     cows.splice(data.index, 1);
     cowsToSheep.push(data.num);
-    setCowsToSheep(cowsToSheep);
+    setCowsToSheep([...cowsToSheep]);
+    setCows(cows.splice(data.num));
     setRenderer(data);
   };
 
   const removeSheep = (data) => {
     sheep.splice(data.index, 1);
     sheepToCows.push(data.num);
-    setSheepToCows(sheepToCows);
+    setSheepToCows([...sheepToCows]);
+    setSheep(sheep.splice(data.num));
     setRenderer(data);
   };
+
   const unmoveSheep = (data) => {
     sheepToCows.splice(data.index, 1);
     sheep.push(data.num);
     setSheep(sheep);
+    setSheepToCows(sheepToCows.splice(data.num));
     setRenderer(data);
   };
+
   const unmoveCow = (data) => {
     cowsToSheep.splice(data.index, 1);
     cows.push(data.num);
     setCows(cows);
+    setCowsToSheep(cowsToSheep.splice(data.num));
     setRenderer(data);
   };
 
-const reset = () => {
-  localStorage.clear()
-  window.location.reload();
-}
-
-  console.log(sheep);
-  console.log(cows);
-  console.log(cowsToSheep);
-  console.log(sheepToCows);
+  const reset = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
 
   return (
     <div>
@@ -110,12 +103,14 @@ const reset = () => {
         </button>
         <br />
         <br />
-        <button className="eBtn" onClick={reset}>Reset</button>
+        <button className="eBtn" onClick={reset}>
+          Reset
+        </button>
       </div>
 
       <div className="leftSide">
         <p className="mainTxt">
-        COWS: {cows.length}
+          COWS: {cows.length}
           {sheepToCows.length > 0 && ", SHEEP: " + sheepToCows.length}
         </p>
         <div className="cowEnclosure">
@@ -131,7 +126,7 @@ const reset = () => {
       </div>
       <div className="rightSide">
         <p className="mainTxt">
-        SHEEP: {sheep.length}
+          SHEEP: {sheep.length}
           {cowsToSheep.length > 0 && ", COWS: " + cowsToSheep.length}
         </p>
         <div className="sheepEnclosure">
